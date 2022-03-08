184e759bc991e3dbbd92a28da62f77f8cb435c0c
Matthew Cambell <snort750@gmail.com>
2022-03-07 19:59:25 -0700

    Added DeathNumber Command

--- services/Discord.js
+++ services/Discord.js
@@ -1,8 +1,13 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const Age = new (require("./Age"));
 
 client.login(process.env.DISCORD_CLIENT_TOKEN);
 
 client.on("message", (message) => {
-  console.log(message);
+  let params = message.content.split(' ');
+    if(params[0].toUpperCase() === '!DEATHNUMBER') {
+      let finalAge = Age.yearCalc({currentAge: params[1], doesSmoke: false, doesWorkout: false})
+      console.log(finalAge);
+    }
 });
